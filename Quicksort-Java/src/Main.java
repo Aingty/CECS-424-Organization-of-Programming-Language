a5461823135584458086d6d6e9c85ae5ffea9801
Aingty Eung <aingtyeung@yahoo.com>
2019-02-24 17:11:56 -0800

    Finished Iterative Quicksort Working on Recursive...

--- Quicksort-Java/src/Main.java
+++ Quicksort-Java/src/Main.java
@@ -45,35 +45,116 @@ public class Main
 
 
 
-
+	////////////////// Iterative Quicksort /////////////////////////////
 	public static void doIteratively(ArrayList<String> input)
 	{
 		System.out.println("Iteratively");
-		ArrayList<String> answer = doIteratively2(input);
+		ArrayList<String> answer = doIteratively2(input, 0, input.size()-1);
 		printResults(answer);
 	}
 
-	public static ArrayList<String> doIteratively2(ArrayList<String> input)
-	{
-		Stack stack = new Stack(); 
-		stack.push(0); 
-		stack.push(input.length); 
-		while (!stack.isEmpty()) 
+	public static ArrayList<String> doIteratively2(ArrayList<String>input, int low, int high) 
+	{ 
+		// Create an array mimicing a "stack" with same size as input size
+		int[] stack = new int[high-low+1]; 
+	
+		// Initialize top of stack 
+		int top = -1; 
+	
+		// Push initial values of low and high to stack 
+		stack[++top] = low; 
+		stack[++top] = high; 
+	
+		// Keep popping from stack
+		while (top >= 0) 
 		{ 
-			int end = stack.pop(); 
-			int start = stack.pop(); 
-			if (end - start < 2) 
-			{
-				continue;
-			}
-			int p = start + ((end - start) / 2); 
-			p = partition(input, p, start, end); 
-			stack.push(p + 1); 
-			stack.push(end); 
-			stack.push(start);
-			stack.push(p); 
+			high = stack[top--]; 
+			low = stack[top--]; 
+	
+			// Call partition method to set the right pivot
+			int p = partition(input, low, high); 
+	
+			// If there are elements on left side of pivot, 
+			// then push left side to stack 
+			if (p-1 > low) 
+			{ 
+				stack[++top] = low; 
+				stack[++top] = p - 1; 
+			} 
+	
+			// If there are elements on right side of pivot, 
+			// then push right side to stack 
+			if (p+1 < high) 
+			{ 
+				stack[++top] = p + 1; 
+				stack[++top] = high; 
+			} 
 		}
-		return input;
+		return input; 
+	}
+
+    public static int partition(ArrayList<String> input, int low, int high) 
+    { 
+        String pivot = input.get(high); 
+          
+        // Index of the small element at any given iteration 
+        int i = (low-1);  
+        for (int j = low; j <= high-1; j++) 
+        { 
+            if (input.get(j).compareTo(pivot) <= 0 )
+            { 
+                i++; 
+                String temp = input.get(i); 
+                input.set(i,input.get(j)); 
+                input.set(j,temp); 
+            } 
+        }
+		// Swapping the pivot index to the small element of index 
+		String temp = input.get(i+1); 
+		input.set(i+1, input.get(high));
+        input.set(high, temp); 
+  
+        return i+1; 
+    } 
+	////////////////////End of Iterative Quicksort//////////////////////////
+
+
+ 	public static void doRecursively(ArrayList<String> input)
+	{
+		System.out.println("Recursively");
+		ArrayList<String> answer = doRecursively2(input);
+		printResults(answer);
+	}
+
+	// Recursive quick sort: Divide array until size is less than 4 and then call insertion sort on it. 
+	// Then combine array into one.
+	public static ArrayList<String> doRecursively2(ArrayList<String> input)
+	{
+		// If size of array is less than 3 then a simple sort is needed
+		if(input.size() <= 3)
+		{
+			return simpleSort(array);
+		}
+		else
+		{
+			ArrayList<String> left = new ArrayList<String>();
+			ArrayList<String> right = new ArrayList<String>();
+			pivot = input.get(input.size()-1);
+			for(int i = 0; i < input.size(); i++)
+			{
+				if(input.get(i).compareTo(pivot) <= 0)
+				{
+					left.add(input.get(i));
+				}
+				else
+				{
+					right.add(input.get(i));
+				}
+			}
+			left = quickSort(left);
+			right = quickSort(right);
+			return left + right;
+		}
 	}
 
 	public static void printResults(ArrayList<String> input)
@@ -85,50 +166,4 @@ public class Main
 		}
 		System.out.println("\n");
 	}
-
-
-	/* This function takes last element as pivot, 
-    places the pivot element at its correct 
-    position in sorted array, and places all 
-    smaller (smaller than pivot) to left of 
-    pivot and all greater elements to right 
-    of pivot */
-    static int partition(int arr[], int low, int high) 
-    { 
-        int pivot = arr[high]; 
-          
-        // index of smaller element 
-        int i = (low-1);  
-        for (int j = low; j <= high-1; j++) 
-        { 
-            // If current element is smaller than or 
-            // equal to pivot 
-            if (arr[j] <= pivot) 
-            { 
-                i++; 
-  
-                // swap arr[i] and arr[j] 
-                int temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
-            } 
-        } 
-  
-        // swap arr[i+1] and arr[high] (or pivot) 
-        int temp = arr[i+1]; 
-        arr[i+1] = arr[high]; 
-        arr[high] = temp; 
-  
-        return i+1; 
-    } 
-/*
- 	public static void doRecursively(ArrayList<String> input)
-	{
-		System.out.println("Recursively");
-		ArrayList<String> answer = doRecursively2(input);
-		printResults(answer);
-	}
-
-
-*/
 }
