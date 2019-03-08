69ed334848ce5d5624a037e079349b0c5f475a0d
Aingty Eung <aingtyeung@yahoo.com>
2019-03-07 16:27:41 -0800

    Work in progress Still can't do iteratively and don't want to google for answers.

--- Homework-2-Permute_Java/src/Main.java
+++ Homework-2-Permute_Java/src/Main.java
@@ -65,7 +65,7 @@ public class Main
         while(startingPos != temp.length()-1)
         {
             // Swapping the main string to get the next possible combinations
-            temp = swap(temp, startingPos, nextPos);
+            temp1 = swap(temp, startingPos, nextPos);
 
             // Adding to array but only if it doesn't already
             if(!array.contains(temp))
@@ -73,13 +73,16 @@ public class Main
                 array.add(temp);
             }
             // Swapping all the possible combination of current string
-            for(int j=1; j < temp.length(); j++)
-            {
-                temp1 = swap(temp,0,j);
-                // Adding to array but only if it doesn't already
-                if(!array.contains(temp1))
+            for(int i=0; i != temp.length()-1; i++)
+            {    
+                for(int j=i+1; j < temp.length(); j++)
                 {
-                    array.add(temp1);
+                    temp1 = swap(temp1,i,j);
+                    // Adding to array but only if it doesn't already
+                    if(!array.contains(temp1))
+                    {
+                        array.add(temp1);
+                    }
                 }
             }
 
@@ -113,4 +116,40 @@ public class Main
         }
         System.out.println();
     }
+
+
+    // Iterative function to generate all permutations of a String in Java
+	// using Collections
+	public static ArrayList<String> permutations(String s)
+	{
+		// create an empty ArrayList to store (partial) permutations
+		ArrayList<String> partial = new ArrayList<String>();
+
+		// initialize the list with the first character of the string
+		partial.add(String.valueOf(s.charAt(0)));
+
+		// do for every character of the specified string
+		for (int i = 1; i < s.length(); i++)
+		{
+			// consider previously constructed partial permutation one by one
+
+			// (iterate backwards to avoid ConcurrentModificationException)
+			for (int j = partial.size() - 1; j >= 0 ; j--)
+			{
+				// remove current partial permutation from the ArrayList
+				String str = partial.remove(j);
+
+				// Insert next character of the specified string in all
+				// possible positions of current partial permutation. Then
+				// insert each of these newly constructed string in the list
+
+				for (int k = 0; k <= str.length(); k++)
+				{
+					// Advice: use StringBuilder for concatenation
+					partial.add(str.substring(0, k) + s.charAt(i) + str.substring(k));
+				}
+			}
+		}
+        return partial;
+	}
 }
