f17a1d8866b537032d988bee6eeef1692032b7f8
Aingty Eung <aingtyeung@yahoo.com>
2019-04-09 10:32:27 -0700

    Renamed this File Now works

---
+++ Homework-3-Permute_Haskell/src/Permutate.hs
@@ -0,0 +1,8 @@
+permutate :: (Eq a) => [a] -> [[a]]
+permutate [] = [[]]
+permutate l = [a:x | a <- l, x <- (permutate $ filter (\x -> x /= a) l)]
+
+main = do
+    print (permutate "abcd")
+    print ("---------------")
+    print (permutate "abc")
--- Homework-3-Permute_Haskell/src/Playground.hs
+++
@@ -1,3 +0,0 @@
-permutate :: (Eq a) => [a] -> [[a]]
-permutate [] = [[]]
-permutate l = [a:x | a <- l, x <- (permutate $ filter (\x -> x /= a) l)]
