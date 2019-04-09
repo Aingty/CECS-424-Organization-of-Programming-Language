87a5ec77daa935325f368baf2a8d0efd93ec97d3
Aingty Eung <aingtyeung@yahoo.com>
2019-04-09 00:29:48 -0700

    Got it work. Have no idea how it works...

--- Homework-3-Permute_Haskell/src/Playground.hs
+++ Homework-3-Permute_Haskell/src/Playground.hs
@@ -1,20 +1,3 @@
-import Debug.Trace
-
-debug = flip trace
-
-permute :: Show a => [a] -> [a] 
-permute xs = 
-    permute' xs xs
-    where 
-        permute' xy xz = case xy of
-            [] -> []
-            (x:xn) ->  permute' (xn) (tail xz ++ [x])  `debug` (show (xz))
-              
-
-permute'' :: [a] -> [[a]]
-permute'' xs =
-    permute''' [xs] xs xs 
-    where
-        permute''' xx xy xz = case xy of
-            [] -> xx
-            (x:xn) -> (permute'''  ( xx ++ [(tail xz ++ [x])])  (xn) (tail xz ++ [x]))
+permutate :: (Eq a) => [a] -> [[a]]
+permutate [] = [[]]
+permutate l = [a:x | a <- l, x <- (permutate $ filter (\x -> x /= a) l)]
