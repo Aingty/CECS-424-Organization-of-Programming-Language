73dcfb1aa9975b2998ec1f161c5dd53b00beae71
Aingty Eung <aingtyeung@yahoo.com>
2019-04-11 09:54:24 -0700

    Updated comments Added the full detail of comments for submission.

--- Homework-3-Permute_Haskell/src/Permutate.hs
+++ Homework-3-Permute_Haskell/src/Permutate.hs
@@ -1,12 +1,36 @@
-permutate :: (Eq a) => [a] -> [[a]]
-permutate [] = [[]]
-permutate l = [a:x | a <- l, x <- (permutate $ filter (\x -> x /= a) l)]
+-- Haskell Permute
+-- CECS 424 Murgolo
+-- Team: Kelly Hall, Victor Kim, Aingty Eung
+-- 04/17/2019
 
+-- :: 		"has type of"
+-- => 		class constraint
+-- filter	keep elements (matching)
+-- <- 		variable assignment or declaration
+-- \x		anonymous function
+-- /=		inequality
+
+
+-- permute has type of [a](list) and returns [[a]](list of lists) with the class constraint that the two values are of equal class and are of equal types
+permute :: (Eq a) => [a] -> [[a]]
+
+-- permute takes a parameter of type list and returns a list of lists
+permute [] = [[]]
+
+-- create an instance of permute called list, and recursively call permute until condition is met
+-- match a:x such that a is declared as list and x is declared as:
+-- in each recursion, permute the filtered values: keep elements of anonymous function x as long as it does not equal a
+permute list = [a:x | a <- list, x <- (permute $ filter (\x -> x /= a) list)]
+
+
+-- print out permutation results for given testers
 main = do
-    putStr ("\nHaskell Permutate String:\na =\n\t\t")
-    print (permutate "a")
+    putStr ("\nHaskell permute String:\na =\n\t\t")
+    print (permute "a")
     putStr ("--------------------------------------------------------------------------------------------\nab =\n\t\t")
-    print (permutate "ab")
+    print (permute "ab")
     putStr ("--------------------------------------------------------------------------------------------\nabcd =\n\t\t")
-    print (permutate "abcd")
+    print (permute "abcd")
     putStr ("\nThe End, GoodBye!\n\n")
+
+ 
